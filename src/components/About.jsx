d1b295615bb585ac4686fed277ea2366ca389f8a
wambeo <samuelwambeo9@gmail.com>
2024-10-01 15:36:38 +0100

    framer motion

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,22 +1,31 @@
 import React from 'react'
 import aboutImg from "../assets/pic2.png"
 import { ABOUT_TEXT } from '../constants'
+import {motion} from "framer-motion"
 
 export default function About() {
   return (
     <div className='border-b border-neutral-900 pb-4'>
         <h2 className='my-20 text-center text-4xl' >About <span className='text-neutral-500'>Me</span> </h2>
         <div className="grid md:grid-cols-2 gap-4">
-            <div className="w-full">
+            <motion.div 
+             whileInView={{opacity: 1, x:0}}
+             initial = {{opacity: 0, x: -100}}
+             transition={{duaration: 0.5}}
+            className="w-full">
                <div className="">
                  <img className='rounded-2xl w-full' src={aboutImg} alt="Esther" />
                </div>              
-            </div>
-            <div className='w-full lg:1/2'>
+            </motion.div>
+            <motion.div 
+             whileInView={{opacity: 1, x:0}}
+             initial = {{opacity: 0, x:100}}
+             transition={{duration: 0.5}}
+            className='w-full lg:1/2'>
                   <div className="flex  items-center text-center">
-                    <p className='my-2 max-w-xl text-xl py-6'>{ABOUT_TEXT}</p>
+                    <p className='my-2 max-w-xl  py-6'>{ABOUT_TEXT}</p>
                   </div>
-               </div>
+               </motion.div>
         </div>
     </div>
   )
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,19 @@
 import React from 'react'
 import { HERO_CONTENT } from '../constants'
 import profilePic from "../assets/pic1.png"
+import {motion} from "framer-motion"
+
+const container = (delay) => ({
+  hidden: { x: -100, opacity: 0},
+  visible:{
+    x:0,
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      delay: delay
+    }
+  }
+})
 
 export default function Hero() {
   return (
@@ -8,15 +21,31 @@ export default function Hero() {
         <div className="grid md:grid-cols-2 gap-4">
             <div className="w-full lg:1/2">
               <div className="flex flex-col items-center lg:items-start">
-                <h1 className='pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl '>Esther Wambeo</h1>
-                <span className='bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent'>Data Scientist</span>
-                <p className='my-2 max-w-xl py-6 font-light tracking-tighter'>{HERO_CONTENT}</p>
+                <motion.h1 
+                  variants = {container(0)}
+                  initial = "hidden"
+                  animate = "visible"
+                 className='pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl '>Esther Wambeo</motion.h1>
+                <motion.span
+                variants = {container(0.5)}
+                initial = "hidden"
+                animate = "visible"
+                className='bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent'>Data Scientist</motion.span>
+                <motion.p 
+                variants = {container(1)}
+                initial = "hidden"
+                animate = "visible"
+                className='my-2 max-w-xl py-6 font-light tracking-tighter'>{HERO_CONTENT}</motion.p>
               
                </div>               
             </div>
             <div className="">
               <div className="">
-                <img src={profilePic} className='w-full' alt="Esther Wambeo" />
+                <motion.img 
+                initial = {{x:100, opacity: 0}}
+                animate = {{x:0, opacity: 1}}
+                transition={{ duration : 1, delay: 1.2}}
+                src={profilePic} className='w-full' alt="Esther Wambeo" />
               </div>
             </div>           
         </div>
